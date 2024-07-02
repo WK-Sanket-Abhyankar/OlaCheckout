05367eafb4d8cbabe120488468f71a657d77691e
Sanket-Abhtankar-webknot <148947472+Sanket-Abhtankar-webknot@users.noreply.github.com>
2024-07-02 17:25:50 +0530

    feature - added footer

--- src/(components)/Sidebar/ColorSelector.tsx
+++ src/(components)/Sidebar/ColorSelector.tsx
@@ -7,12 +7,17 @@ export default function ColorSelector() {
   return (
     <div className={style.color_selector}>
       <div className={style.color_selector_title_container}>
-        <span>Color</span> . <span>title</span>
+        <span>Color</span> . <span>Dark Black</span>
       </div>
       <div className={style.buttons_list}>
         {productColors.map((item: StaticImageData) => (
           <button key={String(item)} className={style.color_button}>
-            <Image src={item} alt={String(item)} role="presentation"  className={style.color_button_image} />
+            <Image
+              src={item}
+              alt={String(item)}
+              role="presentation"
+              className={style.color_button_image}
+            />
           </button>
         ))}
       </div>
--- src/(components)/Sidebar/Sidebar.tsx
+++ src/(components)/Sidebar/Sidebar.tsx
@@ -5,33 +5,37 @@ import ColorSelector from "./ColorSelector";
 import ModelPicker from "./ModelPicker";
 import ProductDetails from "./ProductDetails";
 import MoreLinks from "./MoreLinks";
+import Footer from "./Footer";
 
 export default function Sidebar() {
   return (
     <section className={style.sidebar_main_container}>
-      <div className={style.sb_top_section}>
-        <button className={style.sb_arrow_button}>
-          <Image src={arrowBack} alt="back button" />
-        </button>
-        <label htmlFor="pincode" className={style.pincode_select}>
-          <span>DELIVERING TO</span>
-          <select name="pincode" id="pincode" className={style.select}>
-            <option value="null">Pincode</option>
-          </select>
-        </label>
+      <div className={style.sidebar_main_container_inner}>
+        <div className={style.sb_top_section}>
+          <button className={style.sb_arrow_button}>
+            <Image src={arrowBack} alt="back button" />
+          </button>
+          <label htmlFor="pincode" className={style.pincode_select}>
+            <span>DELIVERING TO</span>
+            <select name="pincode" id="pincode" className={style.select}>
+              <option value="null">Pincode</option>
+            </select>
+          </label>
+        </div>
+        <div>
+          <ColorSelector />
+        </div>
+        <div>
+          <ModelPicker />
+        </div>
+        <div>
+          <ProductDetails />
+        </div>
+        <div>
+          <MoreLinks />
+        </div>
       </div>
-      <div>
-        <ColorSelector />
-      </div>
-      <div>
-        <ModelPicker />
-      </div>
-      <div>
-        <ProductDetails />
-      </div>
-      <div>
-        <MoreLinks />
-      </div>
+      <Footer />
     </section>
   );
 }
