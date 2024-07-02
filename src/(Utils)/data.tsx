fede1b3c2daa011e01f2496054da9b5095292c67
Sanket-Abhtankar-webknot <148947472+Sanket-Abhtankar-webknot@users.noreply.github.com>
2024-07-02 14:10:13 +0530

    setup - added assets and utils

---
+++ src/(Utils)/data.tsx
@@ -0,0 +1,9 @@
+import image1 from "../../public/images/productFront.webp";
+import image2 from "../../public/images/productfrobnt2.webp";
+import image3 from "../../public/images/productfront3.webp";
+import image4 from "../../public/images/productfront4.webp";
+import ad1 from "../../public/images/productad-slide1.svg";
+import ad2 from "../../public/images/productad-slide2.svg";
+
+export const productBikeImages = [image1, image2, image3, image4];
+export const productAdImages = [ad1, ad2]
--- src/(components)/Product/Product.tsx
+++ src/(components)/Product/Product.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import style from "./productad.module.css";
+import Image from "next/image";
+import leftArrow from "../../../public/images/banner-left-arrow.svg";
+import rightArrow from "../../../public/images/banner-right-arrow.svg";
 
 export default function Product() {
   return (
-    <div>Product</div>
-  )
+    <div>
+      <button>
+        <Image src={leftArrow} alt="<" role="presentation" />
+      </button>
+      <button>
+        <Image src={rightArrow} alt="<" role="presentation" />
+      </button>
+      <div className={style.image_container}>
+        
+      </div>
+    </div>
+  );
 }
