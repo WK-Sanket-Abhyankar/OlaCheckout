a6156720a28c5d8aa0eaef36ed97c9506e4f0c16
Sanket-Abhtankar-webknot <148947472+Sanket-Abhtankar-webknot@users.noreply.github.com>
2024-07-02 14:50:38 +0530

    feature - added product slider

--- src/(Utils)/data.tsx
+++ src/(Utils)/data.tsx
@@ -4,6 +4,12 @@ import image3 from "../../public/images/productfront3.webp";
 import image4 from "../../public/images/productfront4.webp";
 import ad1 from "../../public/images/productad-slide1.svg";
 import ad2 from "../../public/images/productad-slide2.svg";
+import { StaticImageData } from "next/image";
 
-export const productBikeImages = [image1, image2, image3, image4];
-export const productAdImages = [ad1, ad2]
+export const productBikeImages: StaticImageData[] = [
+  image1,
+  image2,
+  image3,
+  image4,
+];
+export const productAdImages: StaticImageData[] = [ad1, ad2];
